bee2b1bb6ede7aa0e28f8970ca8e6e8e3696aef3
Samuel-Valentini <samuelvitalia@gmail.com>
2026-01-16 17:09:10 +0100

    first carousel scroll

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,3 +8,26 @@ const menuSpace = document.getElementById("menu-space");
 miniButton.addEventListener("click", () => {
     menuSpace.classList.toggle("display-none-custom");
 });
+
+//
+
+const rightArrow = document.getElementById("right-arrow");
+const leftArrow = document.getElementById("left-arrow");
+
+rightArrow.addEventListener("click", () => {
+    console.log("click right");
+    const images = document.querySelectorAll(".firstturn");
+
+    images.forEach((image) => {
+        image.classList.toggle("display-none-custom-2");
+    });
+});
+
+leftArrow.addEventListener("click", () => {
+    console.log("click left");
+    const images = document.querySelectorAll(".firstturn");
+
+    images.forEach((image) => {
+        image.classList.toggle("display-none-custom-2");
+    });
+});
