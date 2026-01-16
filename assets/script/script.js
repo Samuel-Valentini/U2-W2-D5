eb897c8082597a55de48ce8fa76d4cf3b48552b4
Samuel-Valentini <samuelvitalia@gmail.com>
2026-01-16 16:18:34 +0100

    index first version

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,10 @@
 console.log("script.js online");
+
+// creo un piccolo script per rendere il menu sempre visibile
+
+const miniButton = document.getElementById("mini-button");
+const menuSpace = document.getElementById("menu-space");
+
+miniButton.addEventListener("click", () => {
+    menuSpace.classList.toggle("display-none-custom");
+});
